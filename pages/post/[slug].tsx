1428a60819eaa8c24951e8647a8f720d2c9fd7cd
Mahedi Hasan <mahedi.hs1997@gmail.com>
2020-07-04 12:30:44 +0100

    intial blog commit

---
+++ pages/post/[slug].tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import Layout from "../../components/layout";
+import ReactMarkdown from "react-markdown/with-html";
+
+export default function Post({ content, frontmatter }) {
+  return (
+    <Layout>
+      <article>
+        <ReactMarkdown escapeHtml={false} source={content} />
+      </article>
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  const files = fs.readdirSync("content/posts");
+
+  const paths = files.map((filename) => ({
+    params: {
+      slug: filename.replace(".md", ""),
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const markdownWithMetadata = fs
+    .readFileSync(path.join("content/posts", slug + ".md"))
+    .toString();
+
+  const { data, content } = matter(markdownWithMetadata);
+
+  // Convert post date to format: Month day, Year
+  const options = { year: "numeric", month: "long", day: "numeric" };
+  const formattedDate = data.date.toLocaleDateString("en-US", options);
+
+  const frontmatter = {
+    ...data,
+    date: formattedDate,
+  };
+
+  return {
+    props: {
+      content: `# ${data.title}\n${content}`,
+      frontmatter,
+    },
+  };
+}
